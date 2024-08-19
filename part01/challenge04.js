b6f385dd06968a92a73bd9e6e60a3ba3ca568546
H51M14\Student <Student@gmail.com>
2024-08-19 14:12:28 +0700

    feat: Challenge04

---
+++ part01/challenge04.js
@@ -0,0 +1,12 @@
+function calculateTip(value) {
+    let tip = 0;
+    if (50 <= value <= 300) {
+        tip = 0.15;
+    } else if (value > 300) {
+        tip = 0.2;
+    }
+
+    console.log(`The bill was ${value}, the tip was ${value*tip}, and the total value ${value*(1+tip)}`);
+}
+
+calculateTip(275);
